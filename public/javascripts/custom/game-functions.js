b801e689d4bcce16be3ba678f05eaa29f21cb5f2
axton21 <axton_n@yahoo.com>
2015-05-27 13:59:56 -0400

    done

--- public/javascripts/custom/game-functions.js
+++ public/javascripts/custom/game-functions.js
@@ -1,18 +1,21 @@
 function randomInt(min,max) {
-  // create a range that includes min and max and return a random number from it
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-
+var  prevNum = 0;
 function selectMole() {
-  // here you're going to find what mole is visible
-  // hide the visible mole
-
-  // select a new mole to visible, making sure it isn't the same as the previous one
-  // and make the new mole visible
+  var num = randomInt(1,10);
+  if(num === prevNum){
+    selectMole();
+  }else{
+    $("#mole-num-"+prevNum).hide("slide", { direction: "down" });
+    prevNum = num;
+    $("#mole-num-"+num).show("slide", { direction: "down" });
+  }
 }
 
 // the function below is called on game.js and calls on your selectMole() function every second
 function play() {
   setInterval(function() {
     selectMole();
-  }, 1000);
+  }, 100);
 }
--- public/javascripts/custom/game.js
+++ public/javascripts/custom/game.js
@@ -1,9 +1,12 @@
 $( document ).ready(function() {
-  
+  var score = parseInt($('#counter').text());
 
-  
+
   // create your click handler here
-
+  $('.mole').click(function(){
+    score++;
+    $('#counter').text(score);
+  });
 
 
   // the below code calls on the play function found in game-functions.js
